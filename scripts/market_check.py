1eaa08ccb579f4fadcb47983d5edfc86e87d8dd2
jainendragupta-jpg <jainendragupta@gmail.com>
2026-08-18 19:20:30 +0530

    Update market_check.py

--- scripts/market_check.py
+++ scripts/market_check.py
@@ -1,1 +1,25 @@
-print("Market Alert System Started")
+import os
+import requests
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+message = """
+✅ Market Alert System Started
+
+Repository Connected Successfully
+GitHub Actions Working
+Telegram Working
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Message Sent")
